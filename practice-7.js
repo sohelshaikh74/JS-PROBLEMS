4707a06210a318b7aa1779f8425b9a4d2f8c3587
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-09-15 21:04:30 +0530

    final solve and added new problem

--- practice-7.js
+++ practice-7.js
@@ -4,21 +4,31 @@
 
 //1st way-->
 function printMultiplicationTable(tableOf, tableTill) {
+    //convert to  number first
+    tableOf = Number(tableOf);
+    tableTill = Number(tableTill);
+
+    //validation
+
     if (isNaN(tableOf) || isNaN(tableTill)) {
-        console.log('Invalid input!')
+        console.log('Invalid input! please enter valid number.')
+        return;
     }
-    if (tableOf < 0 || tableTill < 0) {
-        console.log("numbers should be a positive number")
+    if (tableOf <= 0 || tableTill <= 0) {
+        console.log("numbers should be a positive and greater than zero.")
+        return;
     }
     for (let i = 1; i <= tableTill; i++) {
         console.log(`${tableOf}X${i}=${tableOf * i}`)
     }
 }
-// printMultiplicationTable(5, 25)
+//test cases
+printMultiplicationTable(5, 25)
 printMultiplicationTable(5, "fdadfs")
 printMultiplicationTable("dsafd", "fdadfs")
 printMultiplicationTable("dsafd", "5")
 printMultiplicationTable("5", "10")
+printMultiplicationTable("5", -5)
 
 //2nd way-->
 function printMultiplicationTable2(tableOf, tableTill) {
