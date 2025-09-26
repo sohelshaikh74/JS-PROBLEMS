61d4a4c750fca5b715c3fba8022efe63e3cab1a6
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-09-26 21:01:46 +0530

    new problem added

---
+++ practice-12.js
@@ -0,0 +1,2 @@
+// ***********PROBLEM-12***************
+//=============Write a function  that counts and prints the number of  vowels and  consonants in a given string ================
--- practice2.js
+++ practice2.js
@@ -1,21 +1,9 @@
-//write a funciton that returns the result of raising a given number to a specified power
-//eg. 5,3 == 5*5*5=125
-let raising = (base, exponent) => {
-    base = Number(base)
-    exponent = Number(exponent)
-    if (isNaN(base) || isNaN(exponent)) return 'should be a number'
-    if (base < 0 || exponent < 0) return 'should be positive number'
-    let result = 1;
-    for (let i = 1; i <= exponent; i++) {
-        result *= base;
-    }
-    return result
+let mySym = Symbol("kye1")
+let obj = {
+    name: "john",
+    age: 25,
+    [mySym]: "kye 1"
 }
-console.log(raising(2, 3))
-console.log(raising(2, -3))
-console.log(raising(2, "fasd"))
-console.log(raising(0, 0))
-console.log(raising(5, 0))
-console.log(raising(0, 5))
-console.log(raising(0, null))
-console.log(raising(true, 3))
+Object.freeze(obj)
+obj.age = 30
+console.log(obj)
