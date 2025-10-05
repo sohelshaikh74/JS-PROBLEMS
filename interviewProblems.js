40625b42158b581eb1c1458bd0e025f742ec586d
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-10-05 19:49:03 +0530

    new problem added

--- interviewProblems.js
+++ interviewProblems.js
@@ -1,116 +1,120 @@
-// ================ problem --> 1 ======================
-//write a funtion that seperate string,character and number and print as array
-let arr = [100, 'q', 'sohel', 10, 8, 'c']
-let seperate = (arr) => {
-    let character = []
-    let string = [];
-    let number = []
-    arr.forEach(element => {
-        if (typeof element === 'number') {
-            number.push(element)
-        }
-        else if (typeof element === 'string') {
-            if (element.length > 1) {
-                string.push(element)
-            } else character.push(element)
-        }
-    });
-    console.log(number)
-    console.log(string)
-    console.log(character)
-}
-// seperate(arr)
+// // ================ problem --> 1 ======================
+// //write a funtion that seperate string,character and number and print as array
+// let arr = [100, 'q', 'sohel', 10, 8, 'c']
+// let seperate = (arr) => {
+//     let character = []
+//     let string = [];
+//     let number = []
+//     arr.forEach(element => {
+//         if (typeof element === 'number') {
+//             number.push(element)
+//         }
+//         else if (typeof element === 'string') {
+//             if (element.length > 1) {
+//                 string.push(element)
+//             } else character.push(element)
+//         }
+//     });
+//     console.log(number)
+//     console.log(string)
+//     console.log(character)
+// }
+// // seperate(arr)
 
-//================== problem --> 2 ==================
-// merge two object 
-let a = { a: 1 }; let b = { b: 2 }
-// console.log({ ...a, ...b })
-
-
-
-// =================== problemn --> 3===============
-//using funciton currign print sum of two number
-
-let sum = (a, b) => {
-    if (a && b) return a + b;
-    return function (b) {
-        return a + b;
-    }
-
-}
-// console.log(sum(8, 9))
-// console.log(sum(8)(9))
+// //================== problem --> 2 ==================
+// // merge two object 
+// let a = { a: 1 }; let b = { b: 2 }
+// // console.log({ ...a, ...b })
 
 
-// ================== probelm-4 ===============
-// What happens in [] === []
 
-// === checks for strict equality (no type conversion).
+// // =================== problemn --> 3===============
+// //using funciton currign print sum of two number
 
-// Arrays in JavaScript are objects, and objects are compared by reference, not by value.
+// let sum = (a, b) => {
+//     if (a && b) return a + b;
+//     return function (b) {
+//         return a + b;
+//     }
 
-// [] creates a new array object in memory each time.
-
-// So in:
-
-// console.log([] === [])
-
-
-// The first [] is a new array in memory.
-
-// The second [] is another new array in memory.
-
-// They don’t point to the same reference → result is false.
-
-// 🔎 What happens in [] == []
-
-// == does loose equality, but for objects it still compares references, not values.
-
-// Since both [] are different objects in memory, it’s also false.
-
-// ✅ So:
-
-// console.log([] === []) // false
-// console.log([] == [])  // false
-
-// 🔑 Important takeaway
-
-// Arrays/objects are compared by reference, not by contents.
-
-// Only when two variables refer to the exact same array object will it be true:
-
-// let a = []
-// let b = a
-// console.log(a === b) // true (same reference)
-// console.log(a == b)  // true
+// }
+// // console.log(sum(8, 9))
+// // console.log(sum(8)(9))
 
 
-// ⚡Pro tip: If you want to compare arrays by value, you need to manually check their contents, e.g. with .every() or JSON.stringify().
+// // ================== probelm-4 ===============
+// // What happens in [] === []
+
+// // === checks for strict equality (no type conversion).
+
+// // Arrays in JavaScript are objects, and objects are compared by reference, not by value.
+
+// // [] creates a new array object in memory each time.
+
+// // So in:
+
+// // console.log([] === [])
 
 
+// // The first [] is a new array in memory.
+
+// // The second [] is another new array in memory.
+
+// // They don’t point to the same reference → result is false.
+
+// // 🔎 What happens in [] == []
+
+// // == does loose equality, but for objects it still compares references, not values.
+
+// // Since both [] are different objects in memory, it’s also false.
+
+// // ✅ So:
+
+// // console.log([] === []) // false
+// // console.log([] == [])  // false
+
+// // 🔑 Important takeaway
+
+// // Arrays/objects are compared by reference, not by contents.
+
+// // Only when two variables refer to the exact same array object will it be true:
+
+// // let a = []
+// // let b = a
+// // console.log(a === b) // true (same reference)
+// // console.log(a == b)  // true
+
+
+// // ⚡Pro tip: If you want to compare arrays by value, you need to manually check their contents, e.g. with .every() or JSON.stringify().
 
 
 
 
 
-// ====================== problem 1 =======================================
-// reverse each of word 
-let str = "sohel ibrahim shaikh"
-//print hkiahs miharbi lehos
-function reversString(str) {
-    let strArr = str.split(" ").reverse()
-    let result = strArr.map((ele) => {
-        return ele.split("").reverse().join("")
-    })
-    return result.join(" ")
-}
-// console.log(reversString(str))
 
 
-if ([] == []) {
-    console.log("hello")
-} else {
-    console.log("no one is present")
-}
+// // ====================== problem 1 =======================================
+// // reverse each of word 
+// let str = "sohel ibrahim shaikh"
+// //print hkiahs miharbi lehos
+// function reversString(str) {
+//     let strArr = str.split(" ").reverse()
+//     let result = strArr.map((ele) => {
+//         return ele.split("").reverse().join("")
+//     })
+//     return result.join(" ")
+// }
+// // console.log(reversString(str))
 
-//output hello
+
+// if ([] == []) {
+//     console.log("hello")
+// } else {
+//     console.log("no one is present")
+// }
+
+// //output hello
+
+
+// ================================new one ========================
+console.log("john" > 1);
---
+++ problems/practice-20.js
@@ -0,0 +1,36 @@
+// ***********PROBLEM-20***************
+//=============Write a program that prints number from 1 to N, Replacing ================
+//condition:
+// 1) multiple of 3 with  "Fizz" ,
+// 2) Multiple of 5 with 'Buzz',
+// 3) Multiple of  both 3 & 5 with 'FizzBuzz',
+// input: A positive number
+// eg:36 (print 1 to 36 and apply condition over here like 3 and 5 and both )
+// 1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz",16,17,"Fizz",19,"Buzz","Fizz",22,23,"Fizz","Buzz",26,"Fiizz","FizzBuzz",etc
+
+
+function fizzBuzz(inputNumber) {
+    inputNumber = Number(inputNumber)
+    if (isNaN(inputNumber)) {
+        console.log('please enter valid input')
+        return;
+    }
+    if (inputNumber < 0) {
+        console.log('number should be positive')
+        return
+    }
+    if (inputNumber === 0) {
+        console.log("numbre should greater than 0")
+        return;
+    }
+    for (let i = 1; i <= inputNumber; i++) {
+        if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz")
+        else if (i % 3 == 0) console.log('Fizz')
+        else if (i % 5 == 0) console.log("Buzz")
+        else console.log(i)
+    }
+}
+// fizzBuzz(36)
+fizzBuzz(-36)
+// fizzBuzz(0)
+
