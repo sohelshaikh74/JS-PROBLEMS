aaa1f0c0b7871dd588f937bac65a20141d39d3cd
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-11-06 21:29:19 +0530

    problem:new problem added

---
+++ problems/practice-24.js
@@ -0,0 +1,6 @@
+// *********** PROBLEM-24 ****************
+//============= Write a program that checks if two give strings are anagrams of each other ================
+// Input: two string
+// Output:
+// -true: if two strings are anagrams
+// -false: if two strings are not anagrams
